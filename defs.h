e019e31e71ce43453a11b37614771c4f37cd245c
ranniex <deporsam@hotmail.com>
2018-07-07 15:58:55 -0500

    Add files via upload

--- defs.h
+++ defs.h
@@ -121,6 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             cps(void);
+int             chpr(int pid, int priority);
 // swtch.S
 void            swtch(struct context**, struct context*);
 
